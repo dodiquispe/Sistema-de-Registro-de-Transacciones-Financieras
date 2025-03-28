27552829b254b409ccf431197632ea0aa23ab319
Judhit C.Quispe <105253300+dodiquispe@users.noreply.github.com>
2025-03-28 11:13:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from CuentaBancaria import CuentaBancaria
 cuenta = CuentaBancaria("Carlos López", 1000)
 cuenta.depositar(300)
 cuenta.retirar(200)
-cuenta.retirar(1500)  # Fallará por saldo insuficiente.
+cuenta.retirar(1500)
 cuenta.mostrar_saldo()
 cuenta.mostrar_movimientos()
